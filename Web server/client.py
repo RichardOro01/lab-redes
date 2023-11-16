e1ae8839027f8193bdd55e1064cc61c276c376a3
Richard Oropesa <richardoro2001@gmail.com>
2023-11-15 22:59:16 -0500

    feat: clean client code

--- Web server/client.py
+++ Web server/client.py
@@ -1,29 +1,32 @@
+"""Web client."""
 from socket import socket, AF_INET, SOCK_STREAM
 import sys
 
 
-def configureWebServer():
-    serverName = input('Write server host: ')
-    serverPort = int(input('Write server port: '))
-    makeRequest(serverName, serverPort)
+def configure_web_server():
+    """Configure host and post."""
+    server_name = input('Write server host: ')
+    server_port = int(input('Write server port: '))
+    make_request(server_name, server_port)
 
 
-def makeRequest(serverName, serverPort):
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
+def make_request(server_name, server_port):
+    """Make a request to a file."""
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((server_name, server_port))
     sentence = input('Input lowercase sentence: ')
-    print(f'Sending request to {serverName}:{serverPort}')
-    clientSocket.send(f'GET /{sentence} HTTP/1.1'.encode())
-    print(f'Waiting for response from {serverName}:{serverPort}')
-    modifiedSentence = clientSocket.recv(1024)
-    print('From Server: ', modifiedSentence.decode())
-    clientSocket.close()
+    print(f'Sending request to {server_name}:{server_port}')
+    client_socket.send(f'GET /{sentence} HTTP/1.1'.encode())
+    print(f'Waiting for response from {server_name}:{server_port}')
+    response = client_socket.recv(2048)
+    print('From Server: ', response.decode())
+    client_socket.close()
     another = input('Another request? (y,n): ')
     if another == 'y':
-        makeRequest(serverName, serverPort)
+        make_request(server_name, server_port)
     else:
         sys.exit()
 
 
 if __name__ == '__main__':
-    configureWebServer()
+    configure_web_server()
