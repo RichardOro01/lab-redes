6f638bb7c4bc5f6d46ef0e69724d0a8f3b109261
Richard Oropesa <richardoro2001@gmail.com>
2023-11-15 22:51:55 -0500

    feat: client

--- Web server/client.py
+++ Web server/client.py
@@ -1,10 +1,29 @@
-from socket import *
-serverName = 'servername'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-sentence = input('Input lowercase sentence:')
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print('From Server: ', modifiedSentence.decode())
-clientSocket.close()
+from socket import socket, AF_INET, SOCK_STREAM
+import sys
+
+
+def configureWebServer():
+    serverName = input('Write server host: ')
+    serverPort = int(input('Write server port: '))
+    makeRequest(serverName, serverPort)
+
+
+def makeRequest(serverName, serverPort):
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName, serverPort))
+    sentence = input('Input lowercase sentence: ')
+    print(f'Sending request to {serverName}:{serverPort}')
+    clientSocket.send(f'GET /{sentence} HTTP/1.1'.encode())
+    print(f'Waiting for response from {serverName}:{serverPort}')
+    modifiedSentence = clientSocket.recv(1024)
+    print('From Server: ', modifiedSentence.decode())
+    clientSocket.close()
+    another = input('Another request? (y,n): ')
+    if another == 'y':
+        makeRequest(serverName, serverPort)
+    else:
+        sys.exit()
+
+
+if __name__ == '__main__':
+    configureWebServer()
