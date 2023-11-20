d7f4b9834c90212f8deba5e2fde407363f4a57ba
Richard Oropesa <richardoro2001@gmail.com>
2023-11-20 13:00:24 -0500

    fix: ping percent

--- UDP Pinger/udp_pinger_client.py
+++ UDP Pinger/udp_pinger_client.py
@@ -26,6 +26,6 @@ for sequence_number in range(1, PING_AMOUNT+1):
 
 print(f'Packets sended: {PING_AMOUNT}')
 print(f'Packets received: {packets_received}')
-print(f'Packet lost: {(PING_AMOUNT-packets_received)/PING_AMOUNT}%')
+print(f'Packet lost: {(PING_AMOUNT-packets_received)/PING_AMOUNT*100}%')
 
 client_socket.close()
