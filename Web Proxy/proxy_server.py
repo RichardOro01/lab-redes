301526476436b72be6617e1934ad68c0c958da0d
Richard Oropesa <richardoro2001@gmail.com>
2023-11-23 00:08:13 -0500

    feat: proxy working

--- Web Proxy/proxy_server.py
+++ Web Proxy/proxy_server.py
@@ -2,6 +2,7 @@
 from socket import socket, AF_INET, SOCK_STREAM
 import sys
 import re
+import os
 
 PORT = 8888
 
@@ -9,6 +10,9 @@ if len(sys.argv) <= 1:
     print(
         'Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server')
     sys.exit(2)
+
+if not os.path.exists("cache"):
+    os.makedirs("cache")
 
 
 def validate_ip(ip):
@@ -37,45 +41,58 @@ except OSError as error:
     sys.exit(2)
 
 tcpSerSock.listen()
-while True:
-    print(f'Ready to serve on {sys.argv[1]}:{PORT}...')
-    tcpCliSock, addr = tcpSerSock.accept()
-    print('Received a connection from:', addr)
-    message = tcpCliSock.recv(1024)
-    print(message)
-    # Extract the filename from the given message
-    print(message.split()[1])
-    filename = message.split()[1].partition("/")[2]
-    print(filename)
-    file_exist = False
-    file_to_use = "/" + filename
-    print(file_to_use)
-    try:
-        with open(file_to_use[1:], "r", encoding="utf-8") as file:
-            output_data = file.readlines()
-            file_exist = True
-            response = "HTTP/1.0 200 OK\r\nContent-Type:text/html\r\n"
-            response += "\r\n".join(output_data)
-            tcpCliSock.sendall(response.encode())
-            print('Read from cache')
-    except IOError:
-        if not file_exist:
-            web_socket = socket(AF_INET, SOCK_STREAM)
-            hostn = filename.replace("www.", "", 1)
-            print(hostn)
-            try:
-                socket.connect((hostn, 80))
-                fileobj = web_socket.makefile('r', 0)
-                fileobj.write("GET "+"http://" + filename + "HTTP/1.0\n\n")
-                buffer = fileobj.readlines()
-                with open("./" + filename, "wb") as tmpFile:
-                    tmpFile.write("\r\n".join(buffer))
-                tcpCliSock.sendall("\r\n".join(buffer).encode())
-            except:
-                print("Illegal request")
-        else:
-            response = "HTTP/1.0 404 Not Found\r\nContent-Type:text/html\r\n\r\n\r\n404 Not Found"
-            tcpCliSock.sendall(response.encode())
-
+try:
+    while True:
+        print(f'Ready to serve on {sys.argv[1]}:{PORT}...')
+        tcpCliSock, addr = tcpSerSock.accept()
+        print(f'Received a connection from: {addr[0]}:{addr[1]}')
+        message = tcpCliSock.recv(1024)
+        print("Request: ", message)
+        message = message.decode()
+        web_request = message.split()[1]
+        web_request = web_request.strip("/")
+        print("Web Request: ", web_request)
+        request_partition = web_request.partition("/")
+        filename = request_partition[2]
+        hostn = request_partition[0]
+        print("File name: ", filename)
+        file_exist = False
+        file_to_use = "/" + filename
+        print("File to use: ", file_to_use)
+        try:
+            with open(f"cache/{hostn}/{file_to_use[1:]}", "r", encoding="utf-8") as file:
+                output_data = file.readlines()
+                file_exist = True
+                response = "HTTP/1.0 200 OK\r\nContent-Type:text/html\r\n"
+                response += "\r\n".join(output_data)
+                tcpCliSock.sendall(response.encode())
+                print('Read from cache')
+        except IOError:
+            if not file_exist:
+                web_socket = socket(AF_INET, SOCK_STREAM)
+                WEB_SERVER_PORT = 80
+                print(f"Connecting to {hostn}:{WEB_SERVER_PORT}")
+                try:
+                    web_socket.connect((hostn, WEB_SERVER_PORT))
+                    web_socket.sendall(
+                        f"GET http://{filename} HTTP/1.0\n\n".encode())
+                    while True:
+                        buffer = web_socket.recv(1024)
+                        if len(buffer) == 0:
+                            break
+                        if not os.path.exists(f"cache/{hostn}"):
+                            os.makedirs(f"cache/{hostn}")
+                        with open(f"cache/{hostn}/{filename}", "wb") as tmpFile:
+                            tmpFile.write(buffer)
+                        tcpCliSock.sendall(buffer)
+                except Exception as error:
+                    print("Illegal request")
+                    print(error)
+            else:
+                response = "HTTP/1.0 404 Not Found\r\nContent-Type:text/html\r\n\r\n\r\n404 Not Found"
+                tcpCliSock.sendall(response.encode())
+        tcpCliSock.close()
+except InterruptedError:
     tcpSerSock.close()
-    tcpCliSock.close()
+    print("Proxy closed")
+    sys.exit(1)
